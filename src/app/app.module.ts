7218dfe2896dad9100e5833855a283950ecea3d8
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-10-05 17:11:51 -0500

    :pencil: Creating reactive-form component with ng g component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ReactiveFormComponent,
   ],
   imports: [
     BrowserModule,
