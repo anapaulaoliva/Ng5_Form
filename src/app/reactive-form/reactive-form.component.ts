2b41613f9874d8de57a79e8b5fc4330497bc565c
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-10-05 23:21:49 -0500

    :fire: Improving UI with Bootstrap 3 classes

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-reactive-form',
+  selector: 'reactive-form',
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.css']
 })
