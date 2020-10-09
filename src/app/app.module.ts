664acd646ad47125a5f7159615fd6df92052094d
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-10-09 12:14:06 -0500

    :sparkles: Removing unnecesary code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TemplateFormComponent } from './template-form/template-form.component';
 
-import { MatButtonModule, MatCheckboxModule, MatInputModule, MatFormFieldModule, MatSelectModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FieldValidatorDirective } from './field-validator.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent,
-    FieldValidatorDirective
+    TemplateFormComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     FormsModule,
-    ReactiveFormsModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatSelectModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
